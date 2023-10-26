fd7228ce388e3128d3fcf8d2c5d956fa4f4688e5
Nabeel Rana <nabwaregroup@gmail.com>
2023-10-26 11:55:05 -0700

    Remove unused imports

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn, ensureIsAdmin, ensureIsAdminOrCorrectUser } = require("../middleware/auth");
+const { ensureIsAdmin, ensureIsAdminOrCorrectUser } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
